6aee52a8e7da22674c6d81fad09712b694b4a30e
ImJiHwan1 <61612132+ImJiHwan1@users.noreply.github.com>
2020-09-01 22:27:30 +0900

    Create FormCS

---
+++ Application/VisionCS/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace VisionCS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ButtonVision = new System.Windows.Forms.Button();
+            this.ButtonLoadImage = new System.Windows.Forms.Button();
+            this.ButtonClose = new System.Windows.Forms.Button();
+            this.Label_message = new System.Windows.Forms.Label();
+            this.PictureBoxMain = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBoxMain)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ButtonVision
+            // 
+            this.ButtonVision.Location = new System.Drawing.Point(134, 324);
+            this.ButtonVision.Name = "ButtonVision";
+            this.ButtonVision.Size = new System.Drawing.Size(186, 77);
+            this.ButtonVision.TabIndex = 0;
+            this.ButtonVision.Text = "히스토그램 평활화";
+            this.ButtonVision.UseVisualStyleBackColor = true;
+            this.ButtonVision.Click += new System.EventHandler(this.ButtonVision_Click);
+            // 
+            // ButtonLoadImage
+            // 
+            this.ButtonLoadImage.Location = new System.Drawing.Point(326, 324);
+            this.ButtonLoadImage.Name = "ButtonLoadImage";
+            this.ButtonLoadImage.Size = new System.Drawing.Size(183, 37);
+            this.ButtonLoadImage.TabIndex = 1;
+            this.ButtonLoadImage.Text = "이미지 읽어오기";
+            this.ButtonLoadImage.UseVisualStyleBackColor = true;
+            this.ButtonLoadImage.Click += new System.EventHandler(this.ButtonLoadImage_Click);
+            // 
+            // ButtonClose
+            // 
+            this.ButtonClose.Location = new System.Drawing.Point(326, 367);
+            this.ButtonClose.Name = "ButtonClose";
+            this.ButtonClose.Size = new System.Drawing.Size(183, 34);
+            this.ButtonClose.TabIndex = 2;
+            this.ButtonClose.Text = "종료";
+            this.ButtonClose.UseVisualStyleBackColor = true;
+            this.ButtonClose.Click += new System.EventHandler(this.ButtonClose_Click);
+            // 
+            // Label_message
+            // 
+            this.Label_message.AutoSize = true;
+            this.Label_message.Location = new System.Drawing.Point(12, 324);
+            this.Label_message.Name = "Label_message";
+            this.Label_message.Size = new System.Drawing.Size(45, 15);
+            this.Label_message.TabIndex = 3;
+            this.Label_message.Text = "label1";
+            // 
+            // PictureBoxMain
+            // 
+            this.PictureBoxMain.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.PictureBoxMain.Location = new System.Drawing.Point(15, 12);
+            this.PictureBoxMain.Name = "PictureBoxMain";
+            this.PictureBoxMain.Size = new System.Drawing.Size(494, 306);
+            this.PictureBoxMain.TabIndex = 4;
+            this.PictureBoxMain.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(521, 407);
+            this.Controls.Add(this.PictureBoxMain);
+            this.Controls.Add(this.Label_message);
+            this.Controls.Add(this.ButtonClose);
+            this.Controls.Add(this.ButtonLoadImage);
+            this.Controls.Add(this.ButtonVision);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBoxMain)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ButtonVision;
+        private System.Windows.Forms.Button ButtonLoadImage;
+        private System.Windows.Forms.Button ButtonClose;
+        private System.Windows.Forms.Label Label_message;
+        private System.Windows.Forms.PictureBox PictureBoxMain;
+    }
+}
+
---
+++ Application/VisionCS/Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Drawing.Imaging;
+
+namespace VisionCS
+{
+    public partial class Form1 : Form
+    {
+        int m_hDLL;
+        IntPtr intPtr;
+        Bitmap m_bmp;
+
+
+
+        [DllImport("kernel32.dll", EntryPoint = "LoadLibrary")]
+        private extern static int LoadLibrary(string libraryname);
+
+        [DllImport("kernel32.dll", EntryPoint = "GetProcAddress")]
+        private extern static IntPtr GetProcAddress(int hwnd, string proname);
+
+        [DllImport("kernel32.dll", EntryPoint = "FreeLibrary")]
+        private extern static bool FreeLibrary(int hModule);
+
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        delegate int EngineInit();
+
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        delegate int EngineVisionCS
+            (
+                byte[] ret,
+                byte[] gry, int w, int h
+            );
+        public Form1()
+        {
+            unsafe
+            {
+                m_hDLL = LoadLibrary("VisionDll.dll");
+                if (m_hDLL == 0) MessageBox.Show("Error dll load!");
+            }
+            InitializeComponent();
+
+            intPtr = GetProcAddress(m_hDLL, "EngineInit");
+            EngineInit InitEngine = (EngineInit)Marshal.GetDelegateForFunctionPointer(intPtr, typeof(EngineInit));
+            int day = InitEngine();
+            if (day == 0)
+                MessageBox.Show("라이브러리 유효기한이 지났습니다!!");
+            else
+            {
+                String srtMessage = String.Format("유효기한이 {0}일 남았습니다", day);
+                MessageBox.Show(srtMessage);
+            }
+        }
+        private void ButtonVision_Click(object sender, EventArgs e)
+        {
+            if (PictureBoxMain.Image == null) return;
+            int w = m_bmp.Width;
+            int h = m_bmp.Height;
+
+            byte[] col = Get3BytePixelDataFromBitmap(m_bmp, w, h);
+
+            byte[] ret = new byte[w * h * 3];
+            intPtr = GetProcAddress(m_hDLL, "EngineHistogramEqualization");
+            EngineVisionCS HistogramEqualization =
+                (EngineVisionCS)Marshal.GetDelegateForFunctionPointer(intPtr,
+                        typeof(EngineVisionCS));
+            HistogramEqualization(ret, col, w, h);
+
+            Display3BytePixelDataToPictureBox(ret, w, h, 24);
+        }
+
+        private byte[] Get3BytePixelDataFromBitmap(Bitmap bmp, int w, int h)
+        {
+            Rectangle rect = new Rectangle(0, 0, bmp.Width, bmp.Height);
+            System.Drawing.Imaging.BitmapData bmpData =
+                bmp.LockBits(rect, System.Drawing.Imaging.ImageLockMode.ReadOnly,
+                                            bmp.PixelFormat);
+
+            IntPtr ptr = bmpData.Scan0;
+
+            int bytes = Math.Abs(bmpData.Stride) * bmp.Height;
+
+            byte[] colValue = new byte[bytes];
+
+            System.Runtime.InteropServices.Marshal.Copy(ptr, colValue, 0, bytes);
+
+            bmp.UnlockBits(bmpData);
+            byte[] col = new byte[w * h * 3];
+            for (int i = 0; i < w * h; i++)
+            {
+                col[i * 3 + 0] = colValue[i * 4 + 0];
+                col[i * 3 + 1] = colValue[i * 4 + 1];
+                col[i * 3 + 2] = colValue[i * 4 + 2];
+            }
+            return col;
+        }
+
+        private void Display3BytePixelDataToPictureBox(byte[] col, int w, int h, int BitsPerPixel)
+        {
+            int x, y;
+            byte[] raw = new byte[w * h * 4];
+
+            for (y = 0; y < h; y++)
+            {
+                for (x = 0; x < w; x++)
+                {
+                    raw[(4 * w) * (y) + (4 * x) + 0] = col[(3 * w) * (y) + (3 * x) + 0];
+                    raw[(4 * w) * (y) + (4 * x) + 1] = col[(3 * w) * (y) + (3 * x) + 1];
+                    raw[(4 * w) * (y) + (4 * x) + 2] = col[(3 * w) * (y) + (3 * x) + 2];
+                }
+            }
+            Bitmap graybmp = RawTo32BitsBitmap(raw, w, h);
+
+            PictureBoxMain.Image = graybmp;
+            PictureBoxMain.SizeMode = PictureBoxSizeMode.StretchImage;
+            PictureBoxMain.Refresh();
+        }
+
+        private Bitmap RawTo32BitsBitmap(byte[] raw, int w, int h)
+        {
+            Bitmap res = new Bitmap(w, h, PixelFormat.Format32bppRgb);
+            BitmapData data = res.LockBits(
+                                                    new Rectangle(0, 0, w, h),
+                                                    ImageLockMode.ReadOnly,
+                                                    PixelFormat.Format32bppRgb);
+            IntPtr ptr = data.Scan0;
+            Marshal.Copy(raw, 0, ptr, w * h * 4);
+            res.UnlockBits(data);
+            return res;
+        }
+
+
+
+        private void ButtonLoadImage_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog MyOpenFileDialog = new OpenFileDialog();
+            MyOpenFileDialog.ShowDialog();
+            string filePath = MyOpenFileDialog.FileName;
+
+            LoadBitmapDisplayViewScreen(filePath);
+        }
+
+        private void LoadBitmapDisplayViewScreen(string filePath)
+        {
+            Image image = Image.FromFile(filePath);
+
+            PictureBoxMain.Image = image;
+            PictureBoxMain.SizeMode = PictureBoxSizeMode.StretchImage;
+            PictureBoxMain.Refresh();
+
+            Label_message.Text = Path.GetFileName(filePath);
+
+            m_bmp = new Bitmap(image);
+            Label_message.Text = " w= " + m_bmp.Width + " h= " +
+                m_bmp.Height;
+        }
+
+        private void ButtonClose_Click(object sender, EventArgs e)
+        {
+            FreeLibrary(m_hDLL);
+            this.Close();
+        }
+    }
+
+}
